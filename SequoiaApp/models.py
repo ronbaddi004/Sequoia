fb69fb5a197aa517266d2fa9ad90e22d18962e78
Rohan Baddi <ronbaddi004@gmail.com>
2018-02-17 19:49:56 +0530

    Updated model

--- SequoiaApp/models.py
+++ SequoiaApp/models.py
@@ -23,7 +23,7 @@ class Remitter(models.Model):
 
 class Customer(models.Model):
     name = models.CharField(max_length=256)
-    bank_name = models.CharField(max_length=256, blank=False, null=False)
+    bank_name = models.CharField(max_length=250, blank=False, null=False)
     bank_account_number = models.CharField(max_length=256, blank=False, null=False)
     bank_branch_name = models.CharField(max_length=256, blank=False, null=False)
     bank_ifsc_code = models.CharField(max_length=256, blank=False, null=False)
@@ -42,7 +42,7 @@ class Customer(models.Model):
 class RTGS(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="RTGS")
     remitter = models.ForeignKey(Remitter, on_delete=models.CASCADE, related_name="RTGS")
-    cheque_number = models.CharField(max_length=15, blank=False)
+    cheque_number = models.CharField(max_length=16, blank=False)
     amount_in_figure = models.DecimalField(max_digits=10, decimal_places=2)
     amount_in_word = models.CharField(max_length=1024, blank=False, null=False)
     created_on = models.DateTimeField(auto_now_add=True)
